50a975e2a0dd60c6c297938f4ecc17e57915ee9b
Lydia Baik <lydiabaik@gmail.com>
2019-11-15 04:02:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,9 +5,9 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  difference = num2 - num1
-  puts "#{difference}"
-  difference
+  subtracts = num2 - num1
+  puts "#{subtracts}"
+  subtracts
 end
 
 def division(num1, num2)
